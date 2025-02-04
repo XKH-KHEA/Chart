a2739cba3d341e414516a90e5924b912dcd8b544
XKH-KHEA <khea@xtricate.com>
2025-02-04 10:46:36 +0700

    update

--- wwwroot/js/location.js
+++ wwwroot/js/location.js
@@ -1,3 +1,46 @@
+// const connection = new signalR.HubConnectionBuilder()
+//     .withUrl("/locationHub")
+//     .build();
+
+// let map;
+// let userMarkers = {};
+
+// async function initMap() {
+//     map = new google.maps.Map(document.getElementById("map"), {
+//         center: { lat: 0, lng: 0 },
+//         zoom: 3,
+//     });
+
+//     connection.start().then(() => console.log("SignalR Connected"));
+//     trackLocation();
+// }
+
+// function trackLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.watchPosition(position => {
+//             const latitude = position.coords.latitude;
+//             const longitude = position.coords.longitude;
+
+//             connection.invoke("SendLocation", "User1", latitude, longitude)
+//                 .catch(err => console.error(err));
+//         });
+//     } else {
+//         alert("Geolocation is not supported by this browser.");
+//     }
+// }
+
+// connection.on("ReceiveLocation", (user, latitude, longitude) => {
+//     if (!userMarkers[user]) {
+//         userMarkers[user] = new google.maps.Marker({
+//             position: { lat: latitude, lng: longitude },
+//             map: map,
+//             title: user
+//         });
+//     } else {
+//         userMarkers[user].setPosition(new google.maps.LatLng(latitude, longitude));
+//     }
+// });
+
 const connection = new signalR.HubConnectionBuilder()
     .withUrl("/locationHub")
     .build();
@@ -5,13 +48,15 @@ const connection = new signalR.HubConnectionBuilder()
 let map;
 let userMarkers = {};
 
-async function initMap() {
+function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: { lat: 0, lng: 0 },
         zoom: 3,
     });
 
-    connection.start().then(() => console.log("SignalR Connected"));
+    connection.start().then(() => console.log("SignalR Connected"))
+        .catch(err => console.error("SignalR Error:", err));
+
     trackLocation();
 }
 
@@ -22,7 +67,9 @@ function trackLocation() {
             const longitude = position.coords.longitude;
 
             connection.invoke("SendLocation", "User1", latitude, longitude)
-                .catch(err => console.error(err));
+                .catch(err => console.error("SendLocation Error:", err));
+        }, error => {
+            console.error("Geolocation Error:", error.message);
         });
     } else {
         alert("Geolocation is not supported by this browser.");
