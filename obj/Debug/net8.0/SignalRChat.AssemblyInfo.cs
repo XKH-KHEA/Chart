e7fc1761e4090c33c9c4599adc31d2de9e754517
XKH-KHEA <khea@xtricate.com>
2025-02-05 09:05:05 +0700

    update

--- obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa0fbed2395a783559f14499810726d40560673")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820c55ad9d214a04979320e2bfd8f904ed137f96")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,56 +1,86 @@
-﻿// "use strict";
+﻿
 
-// var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+//"use strict";
+//const connection = new signalR.HubConnectionBuilder()
+//    .withUrl("/chatHub")
+//    .build();
 
-// //Disable the send button until connection is established.
-// document.getElementById("sendButton").disabled = true;
+//connection.start().catch(err => console.error(err.toString()));
 
-// connection.on("ReceiveMessage", function (user, message) {
-//     var li = document.createElement("li");
-//     document.getElementById("messagesList").appendChild(li);
-//     // We can assign user-supplied strings to an element's textContent because it
-//     // is not interpreted as markup. If you're assigning in any other way, you 
-//     // should be aware of possible script injection concerns.
-//     li.textContent = `${user} says : ${message}`;
-// });
+//// Send message when button is clicked
+//document.getElementById("sendButton").addEventListener("click", function () {
+//    let user = document.getElementById("userInput").value;
+//    let message = document.getElementById("messageInput").value;
 
-// connection.start().then(function () {
-//     document.getElementById("sendButton").disabled = false;
-// }).catch(function (err) {
-//     return console.error(err.toString());
-// });
+//    if (user && message) {
+//        connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
+//        document.getElementById("messageInput").value = "";
+//    }
+//});
 
-// document.getElementById("sendButton").addEventListener("click", function (event) {
-//     var user = document.getElementById("userInput").value;
-//     var message = document.getElementById("messageInput").value;
-//     connection.invoke("SendMessage", user, message).catch(function (err) {
-//         return console.error(err.toString());
-//     });
-//     event.preventDefault();
-// });
+//// Receive messages
+//connection.on("ReceiveMessage", function (user, message) {
+//    let msg = `<li><strong>${user}:</strong> ${message}</li>`;
+//    document.getElementById("messagesList").innerHTML += msg;
+//});
+
+//// Auto-Save Message Input Every 5 Seconds
+//setInterval(function () {
+//    let message = document.getElementById("messageInput").value;
+//    if (message) {
+//        localStorage.setItem("autoSavedMessage", message);
+//        console.log("Message auto-saved!");
+//    }
+//}, 5000);
+
+//// Restore Auto-Saved Message When Page Reloads
+//window.onload = function () {
+//    let savedMessage = localStorage.getItem("autoSavedMessage");
+//    if (savedMessage) {
+//        document.getElementById("messageInput").value = savedMessage;
+//    }
+//};
 
 "use strict";
+
 const connection = new signalR.HubConnectionBuilder()
     .withUrl("/chatHub")
     .build();
 
 connection.start().catch(err => console.error(err.toString()));
 
+// Request notification permission on page load
+if (Notification.permission !== "granted") {
+    Notification.requestPermission().then(permission => {
+        if (permission === "granted") {
+            console.log("Notifications enabled!");
+        }
+    });
+}
+
 // Send message when button is clicked
 document.getElementById("sendButton").addEventListener("click", function () {
     let user = document.getElementById("userInput").value;
     let message = document.getElementById("messageInput").value;
-    
+
     if (user && message) {
         connection.invoke("SendMessage", user, message).catch(err => console.error(err.toString()));
         document.getElementById("messageInput").value = "";
     }
 });
 
-// Receive messages
+// Receive messages with notification
 connection.on("ReceiveMessage", function (user, message) {
     let msg = `<li><strong>${user}:</strong> ${message}</li>`;
     document.getElementById("messagesList").innerHTML += msg;
+
+    // Show browser notification
+    if (Notification.permission === "granted") {
+        new Notification("New Chat Message", {
+            body: `${user}: ${message}`,
+            icon: "~/images/icon-chart.png" // Optional icon for the notification
+        });
+    }
 });
 
 // Auto-Save Message Input Every 5 Seconds
